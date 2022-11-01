cef445fc45cff322fa0337f80ba0f6b0c6f2a48f
Emmanuel Adeola <emmanueladeola990@gmail.com>
2022-11-01 02:54:15 +0100

    new updates

--- rose.php
+++ rose.php
@@ -91,8 +91,6 @@
     </head>
     <body>
         <div class='head'> 
-            
-        <img src="./images/IMG-20221008-WA0004-removebg-preview.png" width="80px" height="80px" />
 
 
             <h3 id='eh'> Rose of Sharon Group of Schools Akure</h3>
